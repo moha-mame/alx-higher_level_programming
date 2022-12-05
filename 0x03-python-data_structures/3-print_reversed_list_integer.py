5b7004971db9ff00afb6a54c5988fd0cadaff651
moha-mame <>
2022-12-05 08:45:04 +0000

    print alist os Z

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+
+    if my_list is None or len(my_list) == 0:
+        return
+
+    for n in reversed(my_list):
+        print("{:d}".format(n))
