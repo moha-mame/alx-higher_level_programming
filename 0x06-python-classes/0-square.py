e55b8b66d8fd756169c616aca3c697a753d5a448
moha-mame <>
2022-12-20 13:49:31 +0000

    python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""0-square.
+
+This module implements the empty class square for educational purposes.
+
+Example:
+        This module can be called from a main caller function like:
+
+        Square = __import__('0-square').Square
+        my_square = Square()
+
+"""
+
+
+class Square:
+    """Class square as an empty class
+
+    This class is not intended to have anything
+    """
+    pass
