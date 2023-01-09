fdc6728a437100bdb7da7dd540c85c8fef1078e0
moha-mame <>
2023-01-09 16:54:22 +0000

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" base_geometry module """
+
+
+class BaseGeometry:
+    """ BaseGeometry class """
+
+    def area(self):
+        raise Exception("area() is not implemented")
