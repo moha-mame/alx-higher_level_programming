0e2c0fd5e95f53896443eb6765aaf0b52deb81c6
moha-mame <>
2023-01-10 13:28:49 +0000

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
-function that inserts a line of text to a file, after each line containing a specific string
-"""
+"""Module for append_after"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    '''module Search and update
-    '''
-    with open(filename, 'r+') as f:
-        lines = f.readlines()
-        i = 0
-        for line in lines:
-            if line.find(search_string) is not -1:
-                lines.insert(i + 1, new_string)
-            i += 1
-        f.seek(0)
-        f.write("".join(lines))
+    """Inserts a line of text to a file, after each line
+       containing a specific string
+    """
+    output = ''
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            output += line
+            if search_string in line:
+                output += new_string
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(output)
