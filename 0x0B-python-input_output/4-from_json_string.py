7aa7c065173cf8d2bb1c00f12281bb18722e5dfb
moha-mame <>
2023-01-10 10:46:52 +0000

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" from_json_string module """
+import json
+
+
+def from_json_string(my_str):
+    """
+    from_json_string
+
+    Returns an object (Python data structure) represented
+    by a JSON string
+    """
+
+    return json.loads(my_str)
