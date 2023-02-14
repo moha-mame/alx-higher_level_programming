f907ffddc154a8f4565a7f8d99ba686309248a09
moha-mame <>
2023-02-14 11:39:24 +0000

    SQL - Introduction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
--- Create a table called 'first_table' in db 'hbtn_0c_0'
+-- Create a table called 'first_table'
 -- 'first_table' should have (id INT) and (name VARCHAR(256))
--- db name will be passed as argument of mysql cmd
+-- db name will be passed as argument of mysql command
 -- If table 'first_table' already exists, should not fail.
--- Not allowed to use SELECT or SHOW
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
