2146b5c06e53834dfec62e7b50b87165948d2405
moha-mame <>
2023-02-15 08:46:14 +0000

    SQL_More_queries

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Create user 'user_0d_1' with all privileges
+-- Password for user set as 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- Create table 'force_name'
+-- id INT, name VARCHAR(256) can't be null
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
