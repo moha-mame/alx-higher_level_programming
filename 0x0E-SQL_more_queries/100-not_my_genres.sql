f2528bb7ab9e15bcefea4d90df3d2b03fa76d4c3
moha-mame <>
2023-02-16 06:53:03 +0000

    SQL_more_queries

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- Use 'hbtn_0d_tvshows' to list all genres not linked to show 'Dexter'
+-- Results must be sorted ascending order by genre name
+-- You can use max of two SELECT statements
+SELECT g.name
+FROM tv_genres g
+WHERE g.name NOT IN (
+      SELECT g.name
+      FROM tv_genres g
+      INNER JOIN tv_show_genres m ON g.id = m.genre_id
+      INNER JOIN tv_shows s ON m.show_id = s.id
+      WHERE s.title = 'Dexter'
+)
+ORDER BY g.name ASC;
---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- List all Comedy shows in 'hbtn_0d_tvshows'
+-- You can only use one SELECT statement
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres m ON tv_shows.id = m.show_id
+INNER JOIN tv_genres g ON m.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
